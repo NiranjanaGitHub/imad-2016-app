20e1b6cc2d088078eafe08523529b9133318d57b
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 13:18:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,28 @@
-console.log('Loaded!');
-var element = document.getElementById('main');
-element.innerHTML = 'This is Niranjana.V. I have changed the contents.';
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight()
+// Client-Side JavaScript:
+//------------------------
+// console.log('Loaded!'); 
+// var element = document.getElementById('main'); 
+// element.innerHTML = 'This is Niranjana.V. I have changed the contents.'; 
+// var img = document.getElementById('img'); 
+// var marginLeft = 0; //
+// function moveRight() 
+//{
+//    marginLeft = marginLeft + 2;
+//    img.style.marginLeft = marginLeft + 'px';
+//}
+// img.onclick = function ()
+// {
+//    var interval = setInterval(moveRight, 60)
+// }
+
+// Counter Incremant Code:
+//------------------------
+
+var button = document.getElementById('counter');
+button.onclick = function() 
 {
-    marginLeft = marginLeft + 2;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function ()
-{
-    var interval = setInterval(moveRight, 60)
+    
+    counter = counter +1;
+    var span = document.getElementById('count')
+    span.innerHTML = counter.toString();
 }
