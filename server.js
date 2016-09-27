400973e5f88825bb2565f852d0dfa9f552ebb8ff
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 11:03:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res) {
-    res.send('Article One requested and will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }); 
 
 app.get('/article-two', function (req,res) {
-    res.send('Article Two requested and will be served here');
+    res.send('Article One requested and will be served here');
 }); 
 
 app.get('/article-three', function (req,res) {
