d415b4f034b5697d5a992fad3d73830a24800412
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 12:43:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main');
 element.innerHTML = 'This is Niranjana.V. I have changed the contents.';
-var img = document.getElementByID('img');
+var img = document.getElementById('img');
 img.onclick = function ()
 {
     img.style.marginLeft = '100px';
