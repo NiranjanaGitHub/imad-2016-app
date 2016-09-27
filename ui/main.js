728948a0570bb240f31a854a44cf7027507ab8d1
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 13:52:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,9 +28,9 @@ var request = new XMLHttpRequest();
 // Capture the response and store it in a variable:
     request.onreadystatechange = function() 
     {
-        if (request.readyState == XMLHttpRequest.DONE)
+        if (request.readyState === XMLHttpRequest.DONE)
         {
-             if (request.status == 200)
+             if (request.status === 200)
             {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
