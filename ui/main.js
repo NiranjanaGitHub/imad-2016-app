65d6ecf22d5ca26b613579ba42dc06b70c4ab511
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 13:46:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,12 +18,28 @@
 // Counter Incremant Code:
 //------------------------
 
-var counter = 0;
+
 var button = document.getElementById('counter');
-button.onclick = function() 
+
+button.onclick = function()
 {
-    
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+// Create a request object:
+var request = new XMLHttpRequest();
+
+// Capture the response and store it in a variable:
+    request.onreadystatechange = function() 
+    {
+        if (request.readyState === XMLHttpRequest.Done)
+        {
+             if (request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    // Make the request:
+    request.open('GET','http://http://niranjanagithub.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
