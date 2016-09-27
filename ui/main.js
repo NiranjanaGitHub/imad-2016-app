2bcd074923e09c795912209fefe6a8cfc6ad2b58
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-27 12:49:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById('img');
 var marginLeft = 0;
 function moveRight()
 {
-    marginLeft = marginLeft = 2;
+    marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function ()
