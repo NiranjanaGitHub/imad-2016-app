1bef27faa1e3222987fb2fec60d85fffb149c9b6
NiranjanaGitHub <niranjanav159@gmail.com>
2016-09-29 13:13:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -81,39 +81,3 @@ request.open('GET','http://niranjanagithub.imad.hasura-app.io/submit-name?name='
     request.send(null);
 };
 
-// Add Comment box in the article page
-
-var submit = document.getElementById('comment_btn');
-submit.onclick = function() 
-{
-
-// Create a request object:
-var request = new XMLHttpRequest();
-
-// Capture the response and store it in a variable:
-    request.onreadystatechange = function() 
-    {
-        if (request.readyState === XMLHttpRequest.DONE)
-        {
-             if (request.status === 200)
-            {
-                // Capture a list of names and render it as a list
-                var names = request.responseText;
-                names = JSON.parse(names);           // Converting string to JS array.
-                var list = '';
-                for (var i=0; i < names.length; i++)
-                {
-                     list += '<li>' + names[i] + '</li>';
-                }
-                var ul = document.getElementById('commentlist');
-                ul.innerHTML = list;
-            }
-        }
-    };
-
-// Make the request:
-var nameInput = document.getElementById('comment');
-var name = nameInput.value;
-request.open('GET','http://niranjanagithub.imad.hasura-app.io/comment-name?name=' + name,true);
-    request.send(null);
-};
