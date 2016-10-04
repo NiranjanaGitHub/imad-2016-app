183f937d7988df6ff1387efefc04c178f04a3e02
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-04 12:51:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/Aboutme-App/index.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Aboutme-App', 'index.html'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
+
+/* IMAD Practise:
+   -------------
 var articles = 
 {
     'article-one':
@@ -146,7 +157,4 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+*/
