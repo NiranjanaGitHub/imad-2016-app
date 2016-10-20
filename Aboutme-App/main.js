14ae7758b480d56d821cbdb7503477ef8786e614
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-20 17:06:02 +0530

    [imad-console] Updates Aboutme-App/main.js

--- Aboutme-App/main.js
+++ Aboutme-App/main.js
@@ -1,8 +1,23 @@
 console.log('Loaded!'); 
 
-//Code to the move the click Button image:
+//Code to the move the click Button image by 100px using onclick command:
 var click = document.getElementById('click');
+/*
 click.onclick = function()
 {
   click.style.marginLeft='100px';  
 };
+*/
+
+//Code to move the click button image gradually every 100ms using interval command:
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    click.style.marginLeft= marginLeft + 'px';  
+}
+
+img.onclick = function()
+{
+    var interval = setInterval(moveRight,100);
+}
