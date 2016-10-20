2e1d10c5f40f1355f8647cafbf6793cc12bfd94d
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-20 17:28:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/Aboutme-App/Plans', function (req, res) {
 */
 
 var counter = 0;
-app.get('/counter', function (req, res) {
+app.get('Aboutme/counter', function (req, res) {
   counter = counter + 1;    
   res.send(counter.toString());
 });
