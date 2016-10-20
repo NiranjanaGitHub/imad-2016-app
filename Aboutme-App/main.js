527d1afba0576257f19bdabd04083ba1d1475562
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-20 16:56:01 +0530

    [imad-console] Updates Aboutme-App/main.js

--- Aboutme-App/main.js
+++ Aboutme-App/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!'); 
 
 //Code to the move the click Button image:
-var click = document.getElementById('press');
+var click = document.getElementById('click');
 click.onclick = function()
 {
   click.style.marginLeft='100px';  
