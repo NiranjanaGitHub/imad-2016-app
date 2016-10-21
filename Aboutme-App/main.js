75ff92bc5461c692faaa68308334f9cebc562f93
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-21 12:12:44 +0530

    [imad-console] Updates Aboutme-App/main.js

--- Aboutme-App/main.js
+++ Aboutme-App/main.js
@@ -27,7 +27,7 @@ click.onclick = function()
 
 var counter = 0;
 var button = document.getElementById('counter');
-button.onclick = function () 
+button.onclick=function () 
 {
     counter = counter + 1;
     var span = document.getElementById('count');
