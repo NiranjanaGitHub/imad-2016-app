53ce83a74fe53a8bc8e8b4781cea9a90cb307693
NiranjanaGitHub <niranjanav159@gmail.com>
2016-10-27 09:43:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config =
 {
   user: 'niranjanagithub',
   database: 'niranjanagithub',
-  host: 'db.imad.hasura-app.io/',
+  host: 'db.imad.hasura-app.io',
   port: '5432',
   password: process.env.DB_PASSWORD,                                  //Environment variable
 };
